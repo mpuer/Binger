df1ebeb68c3b69e6177922940bb67d16f3bc9fba
Matthew <mattic11@yahoo.com>
2022-01-19 00:22:09 -0500

    fixed csrfProtection & asynchandler require path in routes

--- routes/signup.js
+++ routes/signup.js
@@ -4,7 +4,7 @@ const db = require('../db/models')
 
 const router = express.Router()
 
-const { csrfProtection, asyncHandler } = require('./utils');
+const { csrfProtection, asyncHandler } = require('./util');
 
 router.get('/', asyncHandler(async (req, res) => {
     res.render('sign-up', {title: 'Sign Up'})
