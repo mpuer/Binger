381a1c8152c57af7b36bffa61765fa6379fb7f7e
Chris Britton <chrisbritton790@gmail.com>
2022-01-19 23:49:40 -0800

    added query in browse route handler

--- routes/browse.js
+++ routes/browse.js
@@ -2,12 +2,31 @@ const express = require('express')
 
 const db = require('../db/models')
 
+const { Tvshow } = db;
+
+const { Op } = require('sequelize');
+
 const router = express.Router()
 
 const { csrfProtection, asyncHandler } = require('./util');
 
 router.get('/', asyncHandler(async (req, res) => {
-    const { keyword, cartoons, comedy, documentary, drama, educational, romance, scary } = req.body;
+    const { keyword } = req.body;
+    const data = req.body;
+    const array = Object.values(data);
+
+    if (keyword) {
+        array.shift()
+        const tvShows = await Tvshow.findAll({
+            where: {
+                [Op.and]: [
+                    { name: { [Op.iLike]: `%${keyword}%`} },
+                    { genre: { [Op.or]: array }}
+                ]
+            }
+        })
+
+    }
 
 
     res.render('browse', {title: 'Browse'})
