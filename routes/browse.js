e13f8eb45799a806188e950e265f41104c4344ae
Chris Britton <chrisbritton790@gmail.com>
2022-01-19 22:37:45 -0800

    added to get browse route handler

--- routes/browse.js
+++ routes/browse.js
@@ -7,6 +7,9 @@ const router = express.Router()
 const { csrfProtection, asyncHandler } = require('./util');
 
 router.get('/', asyncHandler(async (req, res) => {
+    const { keyword, cartoons, comedy, documentary, drama, educational, romance, scary } = req.body;
+
+
     res.render('browse', {title: 'Browse'})
 }))
 
