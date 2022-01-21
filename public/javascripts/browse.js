8a2c3b6f224d4a28e4111995ed644f56d973103f
Chris Britton <chrisbritton790@gmail.com>
2022-01-21 12:50:31 -0800

    added select for rating

--- public/javascripts/browse.js
+++ public/javascripts/browse.js
@@ -2,15 +2,18 @@ window.addEventListener("DOMContentLoaded", async (event) => {
     const dataValue = document.getElementById("show-id")
     const textArea = document.getElementById("review");
     const button = document.getElementById("review-button");
+    const select = document.getElementById("rating-select");
     button.addEventListener("click", event => {
         let review = textArea.value;
         let showId = dataValue.value;
+        let rating = select.value;
         const response = await fetch(`http://localhost:8080/browse/${showId}`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify(review),
+            body: JSON.stringify({review, rating}),
         });
+        
     });
 });
