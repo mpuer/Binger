e952ce63fc6b3a361f8ccae9197c195f2c077a6c
Chris Britton <chrisbritton790@gmail.com>
2022-01-21 10:27:03 -0800

    added more to javascript

--- public/javascripts/browse.js
+++ public/javascripts/browse.js
@@ -1,14 +1,19 @@
 window.addEventListener("DOMContentLoaded", async (event) => {
+    const dataValue = document.getElementById("movie-id")
     const textArea = document.getElementById("review");
     const button = document.getElementById("review-button");
     button.addEventListener("click", event => {
         let review = textArea.value;
-        const response = await fetch("http://localhost:8080/browse", {
+        let movieId = dataValue.value;
+        const response = await fetch(`http://localhost:8080/browse/${movieId}`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify(review),
+            body: JSON.stringify({
+                "review": review,
+                "movieId": movieId,
+            }),
         });
     });
 });
