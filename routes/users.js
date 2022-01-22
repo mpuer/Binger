1cb7bf660c95e75767786954a447602b77c576b7
Chris Britton <chrisbritton790@gmail.com>
2022-01-22 15:32:16 -0800

    made improvement to channels rendering

--- routes/users.js
+++ routes/users.js
@@ -14,7 +14,7 @@ const { csrfProtection, asyncHandler } = require('./util');
 router.get('/', asyncHandler(async (req, res) => {
   const channels = await db.Channel.findAll();
   const shows = await db.Tvshow.findAll();
-    
+
   const logged = loggedIn(req, res)
     res.render('users', {
       logged, channels, shows
@@ -27,17 +27,24 @@ router.get('/logout', (req, res) => {
 });
 
 router.post('/', asyncHandler(async (req, res) => {
+  const logged = loggedIn(req, res)
 
-  // const shows = await db.Tvshow.findAll();
-    
-  // const logged = loggedIn(req, res)
-  // const { channelName, showId } = req.body;
-  // // Get channel ie scary shows
-  // console.log(`its here!!!!!!!!!!!!!`, channelName, showId)
-  // const channel = await Channel.create({channelName, showId})
-  // // add tvshowId to that channel
-  // // pull all tvshowIds from channel
-  // // render tvshow.title in our pug 
+  const { channelName, showId } = req.body;
+  // Get channel ie scary shows
+  const photo = null;
+  const channel = await Channel.create({"title": "Not Not Really a Cool Show", "tvShowId": 4, photo});
+  console.log(`its here!!!!!!!!!!!!!`, channel)
+  const shows = await db.Tvshow.findAll({
+    include: [{
+      model: Channel,
+      required: true,
+      where: {title: "Not Not Really a Cool Show"}
+    }]
+  });
+
+  // add tvshowId to that channel
+  // pull all tvshowIds from channel
+  // render tvshow.title in our pug
 
   res.render('users', {
     logged, shows, channel
