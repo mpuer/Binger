acb910e004c6b589ee873db4ea2415020aa63133
Chris Britton <chrisbritton790@gmail.com>
2022-01-23 14:17:45 -0800

    updated tvshow time seeds

--- db/seeders/2-TvShowSeeds.js
+++ db/seeders/2-TvShowSeeds.js
@@ -15,7 +15,7 @@ module.exports = {
       return queryInterface.bulkInsert('Tvshows', [{
         name: 'Bridgerton',
         image: '/images/bridgerton.jpg',
-        showtime: new Date(),
+        showtime: new Date(2022, 1, 24, 20),
         synopsis: 'Wealth, lust, and betrayal set against the backdrop of Regency-era England, seen through the eyes of the powerful Bridgerton family.',
         rating: 2,
         genre: 'Romance',
@@ -25,7 +25,7 @@ module.exports = {
       {
         name: 'Pokemon',
         image: '/images/pokemon.png',
-        showtime: new Date(),
+        showtime: new Date(2022, 1, 25, 7),
         synopsis: 'Ash Ketchum, his yellow pet Pikachu, and his human friends explore a world of powerful creatures.',
         rating: 5,
         genre: 'Cartoons',
@@ -35,7 +35,7 @@ module.exports = {
       {
         name: 'Last Chance U',
         image: '/images/last_chance_u.jpg',
-        showtime: new Date(),
+        showtime: new Date(2022, 1, 25, 21),
         synopsis: 'Intense look inside the world of junior college football, chronicling the stories of players and coaches in the classroom and on the field.',
         rating: 4,
         genre: 'Documentary',
@@ -45,7 +45,7 @@ module.exports = {
       {
         name: 'Spiderman',
         image: '/images/spiderman.jpeg',
-        showtime: new Date(),
+        showtime: new Date(2022, 1, 25, 19),
         synopsis: 'A young man with spider-like abilities fights crime as a superhero in New York City while trying to have a normal personal life.',
         rating: 5,
         genre: 'Cartoons',
@@ -55,7 +55,7 @@ module.exports = {
       {
         name: 'The Haunting of Hill House ',
         image: '/images/the_haunting_of_hill_house.jpg',
-        showtime: new Date(),
+        showtime: new Date(2022, 1, 24, 23),
         synopsis: 'Flashing between past and present, a fractured family confronts haunting memories of their old home and the terrifying events that drove them from it.',
         rating: 4,
         genre: 'Horror',
@@ -65,7 +65,7 @@ module.exports = {
       {
         name: 'Cobra Kai',
         image: '/images/cobra_kai.jpeg',
-        showtime: new Date(),
+        showtime: new Date(2022, 1, 24, 20),
         synopsis: 'Decades after their 1984 All Valley Karate Tournament bout, a middle-aged Daniel LaRusso and Johnny Lawrence find themselves martial-arts rivals again.',
         rating: 3,
         genre: 'Drama',
@@ -75,7 +75,7 @@ module.exports = {
       {
         name: 'The Office',
         image: '/images/the_office.jpg',
-        showtime: new Date(),
+        showtime: new Date(2022, 1, 24, 14, 30),
         synopsis: 'A mockumentary on a group of typical office workers, where the workday consists of ego clashes, inappropriate behavior, and tedium.',
         rating: 5,
         genre: 'Comedy',
@@ -85,7 +85,7 @@ module.exports = {
       {
         name: '30 Rock',
         image: '/images/30_rock.jpg',
-        showtime: new Date(),
+        showtime: new Date(2022, 1, 28, 21),
         synopsis: 'Liz Lemon, head writer of the sketch comedy show "TGS with Tracy Jordan", must deal with an arrogant new boss and a crazy new star, all while trying to run a successful television show without losing her mind.',
         rating: 5,
         genre: 'Comedy',
@@ -95,7 +95,7 @@ module.exports = {
       {
         name: "Dawson's Creek",
         image: '/images/dawsons_creek.jpg',
-        showtime: new Date(),
+        showtime: new Date(2022, 1, 25, 19),
         synopsis: 'Four friends in a small coastal town help each other cope with adolescence.',
         rating: 4,
         genre: 'Romance',
@@ -105,7 +105,7 @@ module.exports = {
       {
         name: 'The Last Dance',
         image: '/images/the_last_dance.jpg',
-        showtime: new Date(),
+        showtime: new Date(2022, 1, 26, 20),
         synopsis: `Charting the rise of the 1990's Chicago Bulls, led by Michael Jordan, one of the most notable dynasties in sports history.`,
         rating: 4,
         genre: 'Documentary',
@@ -115,7 +115,7 @@ module.exports = {
       {
         name: 'Kingdom',
         image: '/images/kingdom.jpg',
-        showtime: new Date(),
+        showtime: new Date(2022, 1, 29, 17),
         synopsis: 'While strange rumors about their ill King grip a kingdom, the crown prince becomes their only hope against a mysterious plague overtaking the land.',
         rating: 5,
         genre: 'Horror',
@@ -125,7 +125,7 @@ module.exports = {
       {
         name: 'Ozark',
         image: '/images/ozark.jpg',
-        showtime: new Date(),
+        showtime: new Date(2022, 1, 27, 15),
         synopsis: 'A financial advisor drags his family from Chicago to the Missouri Ozarks, where he must launder money to appease a drug boss.',
         rating: 4,
         genre: 'Drama',
